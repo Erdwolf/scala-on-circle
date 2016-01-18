c1ebd1f1a8da289598ef58b118e543921a2d40c8
Matthias Reinarz <matthias.reinarz@gmail.com>
2016-01-18 20:12:01 +0000

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-curl --data "{\"tag_name\": \"build-$CIRCLE_BUILD_NUM\"}" https://api.github.com/repos/Erdwolf/scala-on-circle/releases?access_token=$GITHUB_TOKEN > response.json
+curl --data "{\"tag_name\": \"Build #$CIRCLE_BUILD_NUM\"}" https://api.github.com/repos/Erdwolf/scala-on-circle/releases?access_token=$GITHUB_TOKEN > response.json
 cat response.json
 release_id=`python -c 'import json; print json.load(file("response.json"))["id"]'`
-curl -T `find target -name "*.jar"` https://api.github.com/repos/Erdwolf/scala-on-circle/releases/$release_id/assets?name=naked.jar
+curl -T `find target -name "*.jar"` https://api.github.com/repos/Erdwolf/scala-on-circle/releases/$release_id/assets?name=naked.jar&access_token=$GITHUB_TOKEN
