06d5a0568b9d46d0c97301aef0d917c9865da649
Matthias Reinarz <matthias.reinarz@gmail.com>
2016-01-18 13:05:30 +0000

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,1 +1,1 @@
-curl --data '{"tag_name": "v0.0.1"}' https://api.github.com/repos/erdwolf/scala-on-circle/releases?access_token=:your_access_token
+curl --data '{"tag_name": "v0.0.1"}' https://api.github.com/repos/erdwolf/scala-on-circle/releases
